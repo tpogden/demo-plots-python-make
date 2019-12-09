415a15990aae0189fb6b6e0b55b9ecc2754bcb76
Thomas Ogden <thomas.ogden@dunnhumby.com>
2019-12-09 16:57:17 +0000

    Add two plots scripts

---
+++ src/plots/plot_daily_journeys.py
@@ -0,0 +1,69 @@
+"""TODO
+"""
+
+import sys
+import os
+import argparse
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Args ------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser(description="Plotting script.")
+parser.add_argument('--pdf', default=False, action="store_true",
+    help="Output PDF file.")
+parser.add_argument('--png', default=False, action="store_true",
+    help="Output PNG file.")
+parser.add_argument("--show", default=False, action="store_true",
+    help="Show plot in GUI.")
+parser.add_argument("--tex", default=False, action="store_true",
+    help="Render with TeX.")
+opts, unknown = parser.parse_known_args()
+print('opts:', opts)
+
+# Data ------------------------------------------------------------------------
+
+FPATH = 'data/raw/london_merged.csv'
+START_DATE = '2015-01-04'
+END_DATE = '2017-01-03'
+
+df_raw = pd.read_csv(filepath_or_buffer=FPATH, index_col=0, parse_dates=[0], 
+    dtype={'weather_code': np.int, 'is_holiday': np.bool, 'is_weekend': bool, 
+        'season': int})
+
+df = df_raw.loc[START_DATE:END_DATE]
+df['dayofweek'] = df.index.dayofweek
+df['hour'] = df.index.hour
+df['dry'] = df['weather_code'].isin([1,2,3,4])
+
+# Plot ------------------------------------------------------------------------
+
+sns.set_style('darkgrid')
+pal = sns.color_palette('deep')
+if opts.tex:
+    import matplotlib
+    matplotlib.rcParams['text.usetex'] = True
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+sns.lineplot(x='hour', y='cnt', hue='dry', data=df, ax=ax, ci=95, 
+  palette=[pal[0], pal[1]])
+ax.set_xlabel('Hour of Day')
+ax.set_ylabel('Average Trips per Hour')
+ax.set_xticks(range(0, 24))
+ax.set_xlim([0, 23])
+plt.legend(title=None, loc=0, labels=['Wet', 'Dry'])
+
+plt.tight_layout()
+
+# Output ----------------------------------------------------------------------
+
+path_no_ext = os.path.splitext(sys.argv[0])[0]
+if opts.pdf:
+    plt.savefig(path_no_ext + '.pdf')
+if opts.png:
+    plt.savefig(path_no_ext + '.png')
+if opts.show:
+    plt.show()
---
+++ src/plots/plot_trips_vs_temp.py
@@ -0,0 +1,71 @@
+"""TODO
+"""
+
+import sys
+import os
+import argparse
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Args ------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser(description="Plotting script.")
+parser.add_argument('--pdf', default=False, action="store_true",
+    help="Output PDF file.")
+parser.add_argument('--png', default=False, action="store_true",
+    help="Output PNG file.")
+parser.add_argument("--show", default=False, action="store_true",
+    help="Show plot in GUI.")
+parser.add_argument("--tex", default=False, action="store_true",
+    help="Render with TeX.")
+opts, unknown = parser.parse_known_args()
+print('opts:', opts)
+
+# Data ------------------------------------------------------------------------
+
+FPATH = 'data/raw/london_merged.csv'
+START_DATE = '2015-01-04'
+END_DATE = '2017-01-03'
+
+df_raw = pd.read_csv(filepath_or_buffer=FPATH, index_col=0, parse_dates=[0], 
+    dtype={'weather_code': np.int, 'is_holiday': np.bool, 'is_weekend': bool, 
+        'season': int})
+
+df = df_raw.loc[START_DATE:END_DATE]
+df['dayofweek'] = df.index.dayofweek
+df['hour'] = df.index.hour
+df['dry'] = df['weather_code'].isin([1,2,3,4])
+
+df = df[df['hour'] == 8]
+df = df[df['is_weekend'] == False]
+df = df[df['is_holiday'] == False]
+df = df[df['dry'] == True]
+
+# Plot ------------------------------------------------------------------------
+
+sns.set_style('darkgrid')
+pal = sns.color_palette('deep')
+if opts.tex:
+    import matplotlib
+    matplotlib.rcParams['text.usetex'] = True
+
+jp = sns.jointplot(x='t1', y='cnt', data=df, kind='reg', 
+    joint_kws={'scatter_kws':dict(alpha=0.2)}, height=8)
+jp.ax_joint.set_xlabel('Temperature (Feels Like)')
+jp.ax_joint.set_ylabel('Trips per Hour')
+jp.ax_joint.set_ylim([0, 8000])
+
+plt.tight_layout()
+
+# Output ----------------------------------------------------------------------
+
+path_no_ext = os.path.splitext(sys.argv[0])[0]
+if opts.pdf:
+    plt.savefig(path_no_ext + '.pdf')
+if opts.png:
+    plt.savefig(path_no_ext + '.png')
+if opts.show:
+    plt.show()
